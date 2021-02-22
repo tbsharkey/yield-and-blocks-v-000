0352b73981adefcfdb0ce4e9c4d1b028c1eb415f
Tom Sharkey <tbsharkey@gmail.com>
2021-02-22 20:21:49 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,19 +1,7 @@
 def hello_t(array)
-  if block_given?
-    i = 0
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
-    array
-  else
-    puts "Hey! No block was given!"
-  end
-end
+  i = 0
 
-
-["Tim", "Tom", "Jim"].each do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
+  while i < array.length
+    i += 1
   end
 end
